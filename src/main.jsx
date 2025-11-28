f3bd50077f738aef5e8fb219dabfd20458bc3dbe
Jennifer Román <jenniferromanmuerte@gmail.com>
2025-11-28 12:35:05 +0100

    Added router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 // Fichero src/main.jsx
-  import React from 'react'
-  import ReactDOM from 'react-dom/client'
-  import App from './components/App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.jsx";
+import { HashRouter } from "react-router";
 
-
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>,
-  )
+    </HashRouter>
+  </React.StrictMode>
+);
